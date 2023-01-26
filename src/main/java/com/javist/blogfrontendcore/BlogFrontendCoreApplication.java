656b50632a82f4d887298591a2bcfb5f76ec3895
Bohdan Datsenko <reborn.muffin@gmail.com>
2023-01-26 14:09:25 -0800

    initial commit

---
+++ src/main/java/com/javist/blogfrontendcore/BlogFrontendCoreApplication.java
@@ -0,0 +1,13 @@
+package com.javist.blogfrontendcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFrontendCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogFrontendCoreApplication.class, args);
+	}
+
+}
