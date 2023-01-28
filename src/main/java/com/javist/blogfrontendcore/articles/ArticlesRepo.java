735daddb20d1638493039d51b736312eb57f84fc
Bohdan Datsenko <reborn.muffin@gmail.com>
2023-01-28 13:42:06 -0800

    remove unnecessary method declaration

--- src/main/java/com/javist/blogfrontendcore/articles/ArticlesRepo.java
+++ src/main/java/com/javist/blogfrontendcore/articles/ArticlesRepo.java
@@ -1,13 +1,7 @@
 package com.javist.blogfrontendcore.articles;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ArticlesRepo extends JpaRepository<ArticleEntity, Long> {
-    @NonNull
-    List<ArticleEntity> findAll();
-}
+public interface ArticlesRepo extends JpaRepository<ArticleEntity, Long> { }
